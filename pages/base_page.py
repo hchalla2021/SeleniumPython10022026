1452f1823b40e8de89b191ffadb877633b6d09e4
hchalla2021 <53994609+hchalla2021@users.noreply.github.com>
2026-02-10 20:00:58 +0530

    Update base_page.py

--- pages/base_page.py
+++ pages/base_page.py
@@ -10,6 +10,10 @@ class BasePage:
     def __init__(self, driver):
         self.driver = driver
 
+    def __init__(self, driver):
+        self.driver = driver
+        self.wait = WebDriverWait(self.driver, 10)    
+
     def find_element(self, locator):
       return self.driver.find_element(*locator)
 
