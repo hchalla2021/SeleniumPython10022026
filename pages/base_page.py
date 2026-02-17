dcbd34f68667385087448700aa8b1e702267214d
hchalla2021 <53994609+hchalla2021@users.noreply.github.com>
2026-02-17 22:12:54 +0530

    Update base_page.py

--- pages/base_page.py
+++ pages/base_page.py
@@ -2,7 +2,7 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.select import Select
 import os
-# this is added in local1
+# This is local change
 class BasePage:
     def __init__(self, driver):
         self.driver = driver
